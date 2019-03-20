890d59c840718d441a5dc15c106deff4af1c94f9
RamitSharma991 <RamitsMac@Ramits-MacBook-Pro.local>
2019-03-20 14:46:49 +0530

    initial commit

---
+++ RNDM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RNDM
+//
+//  Created by Ramit sharma on 20/03/19.
+//  Copyright © 2019 Ramit sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
